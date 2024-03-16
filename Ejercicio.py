c4064e6b8ffd42ec9f07b3d60fc4792091b9ced3
Richard <147564855+jr0612@users.noreply.github.com>
2024-03-15 21:37:25 -0500

    Update Ejercicio.py

--- Ejercicio.py
+++ Ejercicio.py
@@ -1,3 +1,4 @@
+import math
 class Point:
     definition: str = "Entidad geometrica abstracta que representa una ubicación en un espacio."
 
@@ -17,24 +18,47 @@ class Point:
         distance = ((self.x - point.x)**2+(self.y - point.y)**2)**(0.5)
         return distance
 
-'''
+
 class Line:
-    def _init_(self, start: Point, end: Point):
+    def __init__(self, start: Point, end: Point):
         self.start = start
         self.end = end
 
-    def compute_lenght(self):
+    def compute_length(self):
         return self.start.compute_distance(self.end)
 
     def compute_slope(self) -> float:
-        return (self.start.y - self.end.y) / (self.start.x - self.end.x)
+        if self.start.y == self.end.y:
+            if self.start.x > self.end.x:
+                return 180
+            else:
+                return 0
+        elif self.start.x == self.end.x:
+            if self.start.y > self.end.y:
+                return 270
+            else:
+                return 90
+        else:
+            
+            component_y = self.end.y - self.start.y
+            component_x = self.end.x - self.start.x
+            radians_slope = math.atan(component_y / component_x)
+            return math.degrees(radians_slope)
 
     def compute_horizontal_cross(self) -> bool:
+        if self.start.y <= 0 and self.end.y > 0 or self.end.y <= 0 and self.start.y > 0:
+            return True
+        else:
+            return False
 
 
 
     def compute_vertical_cross(self) -> bool:
-'''
+        if self.start.x <= 0 and self.end.x > 0 or self.end.x <= 0 and self.start.x > 0:
+            return True
+        else:
+            return False
+
 
 class Rectangle:
 
@@ -62,6 +86,38 @@ class Rectangle:
         self.width = (top_right.x - bottom_left.x)
         self.bottom_left = bottom_left
         self.top_right = top_right
+    
+    
+    def fourth_method(self, first_line: Line, second_line: Line, third_line: Line, fourth_line: Line):
+        self.width = first_line.compute_length()
+        self.height = second_line.compute_length()
+
+        self.bottom_left = first_line.start
+        self.top_right = third_line.end
+
+    def are_valid_rectangle_lines(self, first_line: Line, second_line: Line, third_line: Line, fourth_line: Line) -> bool:
+        
+        if not (self.are_perpendicular(first_line, second_line) and
+                self.are_perpendicular(second_line, third_line) and
+                self.are_perpendicular(third_line, fourth_line)):
+            return False
+
+        
+        length1 = first_line.compute_length()
+        length2 = second_line.compute_length()
+        length3 = third_line.compute_length()
+        length4 = fourth_line.compute_length()
+
+        return length1 == length3 and length2 == length4
+
+    def are_perpendicular(self, line1: Line, line2: Line) -> bool:
+        
+        slope1 = line1.compute_slope()
+        slope2 = line2.compute_slope()
+
+    
+        return slope1 + slope2 == 90 or slope1 + slope2 == 270
+
         
 
 
@@ -83,30 +139,24 @@ class Rectangle:
 
 
 class Square(Rectangle):
-    def __init__(self, method: int = 1, side_length: float = 0, bottom_left: Point = None, center: Point = None, top_right: Point = None):
+    def __init__(self, method: int = 1, side_length: float = 0, bottom_left: Point = None, top_right: Point = None):
         if method == 1:
             super().__init__(method=1, width=side_length, height=side_length, bottom_left=bottom_left)
         elif method == 2:
-            super().__init__(method=2, width=side_length, height=side_length, center=center)
+            super().__init__(method=2, width=side_length, height=side_length)
         elif method == 3:
             super().__init__(method=3, bottom_left=bottom_left, top_right=top_right)
 
         self.side_length = side_length
 
-    def are_valid_square_points(point1: Point, point2: Point) -> bool:
+    def are_valid_square_points(self, point1: Point, point2: Point) -> bool:
+        distancia1 = point2.x - point1.x
+        distancia2 = point2.y - point1.y
         
-        side_length = abs(point1.x - point2.x)
-        if abs(point1.y - point2.y) != side_length:
-            return False
-
+        print(distancia1)
+        print(distancia2)
         
-        if point1.x == point2.x or point1.y == point2.y:
-            return True
-        else:
-            return False
-
-
-
+        return distancia1 == distancia2
 
 
 def main():
@@ -122,6 +172,7 @@ def main():
             print("1. Vértice inferior izquierdo + ancho y alto")
             print("2. Centro + ancho y alto")
             print("3. Vértice inferior izquierdo y vértice superior derecho")
+            print("4. cuatro lineas")
 
             rect_choice = int(input("Ingrese el número de la opción: "))
             if rect_choice == 1:
@@ -133,7 +184,14 @@ def main():
                 rectangle.first_method(bottom_left=Point(bottom_left_x, bottom_left_y), height=height, width=width)
                 print(f"El perímetro del rectángulo es: {rectangle.compute_perimeter()}")
                 print(f"El área del rectángulo es: {rectangle.compute_area()}")
-                break
+                print("/nVERIFICAR INTERFERENCIA DE PUNTO")
+                point_x = float(input("Ingrese la coordenada x del punto: "))
+                point_y =float(input("Ingrese la coordenada y del punto: "))
+                if rectangle.compute_interference_point(Point(point_x, point_y)):
+                    print("el punto esta adentro de la figura")
+                else:
+                    print("el punto NO esta adentro de la figura")
+               
             elif rect_choice == 2:
                 center_x = float(input("Ingrese la coordenada x del centro: "))
                 center_y = float(input("Ingrese la coordenada y del centro: "))
@@ -143,7 +201,14 @@ def main():
                 rectangle.second_method(center=Point(center_x, center_y), height=height, width=width)
                 print(f"El perímetro del rectángulo es: {rectangle.compute_perimeter()}")
                 print(f"El área del rectángulo es: {rectangle.compute_area()}")
-                break
+                print("/nVERIFICAR INTERFERENCIA DE PUNTO")
+                point_x = float(input("Ingrese la coordenada x del punto: "))
+                point_y =float(input("Ingrese la coordenada y del punto: "))
+                if rectangle.compute_interference_point(Point(point_x, point_y)):
+                    print("el punto esta adentro de la figura")
+                else:
+                    print("el punto NO esta adentro de la figura")
+                
             elif rect_choice == 3:
                 bottom_left_x = float(input("Ingrese la coordenada x del vértice inferior izquierdo: "))
                 bottom_left_y = float(input("Ingrese la coordenada y del vértice inferior izquierdo: "))
@@ -153,7 +218,58 @@ def main():
                 rectangle.third_method(bottom_left=Point(bottom_left_x, bottom_left_y), top_right=Point(top_right_x, top_right_y))
                 print(f"El perímetro del rectángulo es: {rectangle.compute_perimeter()}")
                 print(f"El área del rectángulo es: {rectangle.compute_area()}")
-                break
+                print("/nVERIFICAR INTERFERENCIA DE PUNTO")
+                point_x = float(input("Ingrese la coordenada x del punto: "))
+                point_y =float(input("Ingrese la coordenada y del punto: "))
+                if rectangle.compute_interference_point(Point(point_x, point_y)):
+                    print("el punto esta adentro de la figura")
+                else:
+                    print("el punto NO esta adentro de la figura")
+             
+            elif rect_choice ==4:
+
+                print("Ingrese las coordenadas para las cuatro líneas que formarán el rectángulo:")
+                point1_x = float(input("Ingrese la coordenada x del primer punto de la línea superior: "))
+                point1_y = float(input("Ingrese la coordenada y del primer punto de la línea superior: "))
+                point2_x = float(input("Ingrese la coordenada x del segundo punto de la línea superior: "))
+                point2_y = float(input("Ingrese la coordenada y del segundo punto de la línea superior: "))
+                line1 = Line(Point(point1_x, point1_y), Point(point2_x, point2_y))
+
+                point3_x = float(input("Ingrese la coordenada x del primer punto de la línea izquierda: "))
+                point3_y = float(input("Ingrese la coordenada y del primer punto de la línea izquierda: "))
+                point4_x = float(input("Ingrese la coordenada x del segundo punto de la línea izquierda: "))
+                point4_y = float(input("Ingrese la coordenada y del segundo punto de la línea izquierda: "))
+                line2 = Line(Point(point3_x, point3_y), Point(point4_x, point4_y))
+
+                point5_x = float(input("Ingrese la coordenada x del primer punto de la línea inferior: "))
+                point5_y = float(input("Ingrese la coordenada y del primer punto de la línea inferior: "))
+                point6_x = float(input("Ingrese la coordenada x del segundo punto de la línea inferior: "))
+                point6_y = float(input("Ingrese la coordenada y del segundo punto de la línea inferior: "))
+                line3 = Line(Point(point5_x, point5_y), Point(point6_x, point6_y))
+
+                point7_x = float(input("Ingrese la coordenada x del primer punto de la línea derecha: "))
+                point7_y = float(input("Ingrese la coordenada y del primer punto de la línea derecha: "))
+                point8_x = float(input("Ingrese la coordenada x del segundo punto de la línea derecha: "))
+                point8_y = float(input("Ingrese la coordenada y del segundo punto de la línea derecha: "))
+                line4 = Line(Point(point7_x, point7_y), Point(point8_x, point8_y))
+
+
+                rectangle = Rectangle()
+                if not rectangle.are_valid_rectangle_lines(line1, line2, line3, line4):
+                    print("Las líneas no forman un rectángulo válido.")
+                    continue
+                rectangle.fourth_method(line1, line2, line3, line4)
+
+                print(f"El perímetro del rectángulo es: {rectangle.compute_perimeter()}")
+                print(f"El área del rectángulo es: {rectangle.compute_area()}")
+                print("/nVERIFICAR INTERFERENCIA DE PUNTO")
+                point_x = float(input("Ingrese la coordenada x del punto: "))
+                point_y =float(input("Ingrese la coordenada y del punto: "))
+                if rectangle.compute_interference_point(Point(point_x, point_y)):
+                    print("el punto esta adentro de la figura")
+                else:
+                    print("el punto NO esta adentro de la figura")
+     
             else:
                 print("Opción no válida")
         elif main_choice == 2:
@@ -170,27 +286,52 @@ def main():
                 square = Square(method=1, side_length=side_length, bottom_left=Point(bottom_left_x, bottom_left_y))
                 print(f"El perímetro del cuadrado es: {square.compute_perimeter()}")
                 print(f"El área del rectángulo es: {square.compute_area()}")
+                print("/nVERIFICAR INTERFERENCIA DE PUNTO")
+                point_x = float(input("Ingrese la coordenada x del punto: "))
+                point_y =float(input("Ingrese la coordenada y del punto: "))
+                if square.compute_interference_point(Point(point_x, point_y)):
+                    print("el punto esta adentro de la figura")
+                else:
+                    print("el punto NO esta adentro de la figura")
             elif square_choice == 2:
                 center_x = float(input("Ingrese la coordenada x del centro: "))
                 center_y = float(input("Ingrese la coordenada y del centro: "))
                 side_length = float(input("Ingrese la longitud del lado del cuadrado: "))
-                square = Square(method=2, side_length=side_length, center=Point(center_x, center_y))
+                square = Square(method=2, side_length=side_length)
+                square.second_method(center=Point(center_x, center_y), height=side_length, width=side_length)
                 print(f"El perímetro del cuadrado es: {square.compute_perimeter()}")
                 print(f"El área del rectángulo es: {square.compute_area()}")
+                print("/nVERIFICAR INTERFERENCIA DE PUNTO")
+                point_x = float(input("Ingrese la coordenada x del punto: "))
+                point_y =float(input("Ingrese la coordenada y del punto: "))
+                if square.compute_interference_point(Point(point_x, point_y)):
+                    print("el punto esta adentro de la figura")
+                else:
+                    print("el punto NO esta adentro de la figura")
             elif square_choice == 3:
                 bottom_left_x = float(input("Ingrese la coordenada x del vértice inferior izquierdo: "))
                 bottom_left_y = float(input("Ingrese la coordenada y del vértice inferior izquierdo: "))
                 top_right_x = float(input("Ingrese la coordenada x del vértice superior derecho: "))
                 top_right_y = float(input("Ingrese la coordenada y del vértice superior derecho: "))
+
+                square = Square(method=3, bottom_left=Point(bottom_left_x, bottom_left_y), top_right=Point(top_right_x, top_right_y))
+                square.third_method(bottom_left=Point(bottom_left_x, bottom_left_y), top_right=Point(top_right_x, top_right_y))
              
  
-                if not Square.are_valid_square_points(Point(bottom_left_x, bottom_left_y), Point(top_right_x, top_right_y)):
+                if not square.are_valid_square_points(Point(bottom_left_x, bottom_left_y), Point(top_right_x, top_right_y)):
                     print("Los puntos ingresados no forman un cuadrado válido.")
                     continue
-                
-                square = Square(method=3, bottom_left=Point(bottom_left_x, bottom_left_y), top_right=Point(top_right_x, top_right_y))
+
                 print(f"El perímetro del cuadrado es: {square.compute_perimeter()}")
-                print(f"El área del rectángulo es: {square.compute_area()}")                  
+                print(f"El área del rectángulo es: {square.compute_area()}")
+                print("/nVERIFICAR INTERFERENCIA DE PUNTO")
+                point_x = float(input("Ingrese la coordenada x del punto: "))
+                point_y =float(input("Ingrese la coordenada y del punto: "))
+                if square.compute_interference_point(Point(point_x, point_y)):
+                    print("el punto esta adentro de la figura")
+                else:
+                    print("el punto NO esta adentro de la figura")
+                  
             else:
                 print("Opción no válida")           
                 continue
